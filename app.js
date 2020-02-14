6456899958f768b7ead6625f91fa13975537753d
Talha <talha.zia>
2020-02-14 15:48:37 +0500

    served a response reading from file and showing it to browser

---
+++ app.js
@@ -0,0 +1,17 @@
+const http = require('http');
+var fs = require("fs");
+
+const hostname = '127.0.0.1';
+const port = 4000;
+
+const data =fs.readFileSync('web/html/index.html', 'utf8');
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.end(data);
+  });
+  
+  server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+  });
